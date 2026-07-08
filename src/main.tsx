6db5af16ee47a7a42ac18b62697b69f05e22bd54
nbh-2695 <huanbanguyen1530@gmail.com>
2026-07-08 20:05:10 +0700

    feat(FE): Responsive + polish UI trang BDS + search nang cao

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 import { AuthProvider } from './contexts/AuthContext';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
